723c4670203645efa22af7414b1a5d308a5350f4
Kunal-ryfl <kunalsharma070806@gmail.com>
2023-10-08 17:44:02 +0530

    ok

---
+++ app/(site)/(routes)/root/[folderId]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return (
+    <div className=" py-[2rem] container">
+      <p>Loading...</p>
+    </div>
+  );
+}
